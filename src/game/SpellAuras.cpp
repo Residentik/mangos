903f5f249bf8942e764f46b03217fa2807f164d0
michalpolko <michalpolko@o2.pl>
2012-01-01 16:27:51 +0100

    [a169] Fixed duration of Unbound Plague after passing to new target. Aura still doesn't apply to targets with Plague Sickness. :(
    Conflicts:
    
    	src/shared/revision_andeeria_nr.h

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6583,8 +6583,7 @@ void Aura::HandlePeriodicDamage(bool apply, bool Real)
                     if (SpellAuraHolderPtr holder = pCaster->GetSpellAuraHolder(GetId()))
                     {
                         GetHolder()->SetAuraDuration(holder->GetAuraDuration());
-                        GetHolder()->RefreshHolder();
-                        pCaster->RemoveAurasDueToSpell(GetId());
+                        GetHolder()->SendAuraUpdate(false);
                     }
                 }
             }
